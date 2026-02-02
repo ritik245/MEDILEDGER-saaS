d4d8eca8258883f633e9d9df93aaa644ba927dd7
kartik <kaxxtric001@gmail.com>
2026-02-02 21:57:35 +0530

    Added encryption+hashing+record service layer implememted

---
+++ Backend/src/services/record.service.ts
@@ -0,0 +1,24 @@
+import  fs from "fs";
+import { prisma } from "../lib/prisma";
+import { encryptFile,hashFile } from "../utils/encryption";
+
+export async function saveRecord(filePath:string,patientId:number){
+    const encryptedPath=filePath+".enc";
+
+    //encrypt
+    encryptFile(filePath,encryptedPath);
+    //hash
+    const hash= await hashFile(encryptedPath);
+
+    //delete the original
+    fs.unlinkSync(filePath);
+    //savve in db
+    const record=await prisma.record.create({
+        data:{
+            cid:encryptedPath,
+            hash,
+            patientId,
+        },
+    });
+    return record;
+}
